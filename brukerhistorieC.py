3a9d538b228a9709e27c0502c8bc229294e7d3ee
Jakob Opland <jakobop@stud.ntnu.no>
2023-03-16 13:59:13 +0100

    starte på brukerhistorie c

--- brukerhistorieC.py
+++ brukerhistorieC.py
@@ -2,14 +2,40 @@ import sqlite3
 con = sqlite3.connect('jernbane.db')
 cursor = con.cursor()
 
-cursor.execute('''INSERT INTO Banestrekning VALUES ('Test1', 'Energi', 3, 'TestStart', 'TestSlutt')''')
-con.commit()
-
 # Brukerhistorie C 
-def togruterInnomStasjon(ukedag):
-    #cursor.execute("SELECT * FROM  WHERE navn =:navn", {"navn" = ukedag})
-    #row = cursor.fetchall()
-    print("First row from table person:")
-    #print(row)
+
+# Legge inn data i  Ukedager-tabellen
+#hverdager = ["Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag"]
+#alleDager = ["Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag"]
+#
+#for i in hverdager:
+#    cursor.execute(f'''INSERT INTO Ukedager VALUES (1, '{i}')''')
+#    cursor.execute(f'''INSERT INTO Ukedager VALUES (3, '{i}')''')
+#for i in alleDager:
+#    cursor.execute(f'''INSERT INTO Ukedager VALUES (2, '{i}')''')
+
+
+def togruterInnomStasjon(ukedag, stasjonsNavn):
+    togruterInnomStasjonPåUkedag = cursor.execute("""
+        SELECT Togrute.TogruteID 
+        FROM Togrute INNER JOIN Ukedager
+             ON Togrute.TogruteID = Ukedager.TogruteID
+        WHERE Ukedager.Ukedag = :ukedag AND Togrute.TogruteID IN (
+            SELECT TogruteID
+            FROM Togrute NATURAL JOIN StasjonerIRute
+            WHERE StasjonerIRute.JernbanestasjonNavn = :stasjonsNavn
+        )
+        """, {"ukedag": ukedag, "stasjonsNavn": stasjonsNavn})
+    
+    rows = togruterInnomStasjonPåUkedag.fetchall()
+    print(f"Togrutene som går innom {stasjonsNavn} {ukedag}")
+    for row in rows:
+        print(row)
+
+togruterInnomStasjon("Lørdag", "Bodø")
+
+
+
+con.commit()
 
 

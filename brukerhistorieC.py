b1e30a2fe4d27a5de9087f04318b9c4e93dee141
Jakob Opland <jakobop@stud.ntnu.no>
2023-03-19 11:25:44 +0100

    start on usecase c

--- brukerhistorieC.py
+++ brukerhistorieC.py
@@ -14,10 +14,9 @@ cursor = con.cursor()
 #for i in alleDager:
 #    cursor.execute(f'''INSERT INTO Ukedager VALUES (2, '{i}')''')
 
-
 def togruterInnomStasjon(ukedag, stasjonsNavn):
     togruterInnomStasjonPåUkedag = cursor.execute("""
-        SELECT Togrute.TogruteID 
+        SELECT Togrute.TogruteID, Togrute.OperatorNavn
         FROM Togrute INNER JOIN Ukedager
              ON Togrute.TogruteID = Ukedager.TogruteID
         WHERE Ukedager.Ukedag = :ukedag AND Togrute.TogruteID IN (
@@ -26,14 +25,32 @@ def togruterInnomStasjon(ukedag, stasjonsNavn):
             WHERE StasjonerIRute.JernbanestasjonNavn = :stasjonsNavn
         )
         """, {"ukedag": ukedag, "stasjonsNavn": stasjonsNavn})
-    
     rows = togruterInnomStasjonPåUkedag.fetchall()
-    print(f"Togrutene som går innom {stasjonsNavn} {ukedag}")
+    
+    
     for row in rows:
-        print(row)
+        startstasjon = cursor.execute("""
+            SELECT JernbanestasjonNavn, Avgangstid
+            FROM StasjonerIRute
+            WHERE TogruteID = :row AND Ankomsttid IS NULL
+        """, {"row": row[0]})
+
+        startstasjonForTogrute = startstasjon.fetchall()
+
+        sluttstasjon = cursor.execute("""
+            SELECT JernbanestasjonNavn, Ankomsttid
+            FROM StasjonerIRute
+            WHERE TogruteID = :row AND Avgangstid IS NULL
+        """, {"row": row[0]})
+
+        sluttstasjonForTogrute = startstasjon.fetchall()
+
+        print(f'''Togruten med id: {row[0]} og operatør {row[1]}, 
+går innom {stasjonsNavn} på {ukedag}er. 
+Den starter fra {startstasjonForTogrute[0][0]} kl {startstasjonForTogrute[0][1]}, 
+og ankommer {sluttstasjonForTogrute[0][0]} kl {sluttstasjonForTogrute[0][1]}''')
 
 togruterInnomStasjon("Lørdag", "Bodø")
-
 
 
 con.commit()

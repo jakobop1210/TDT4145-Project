e1baad62cab1613be1025ce78de675879789f512
Jakob Opland <jakobop@stud.ntnu.no>
2023-03-23 12:43:49 +0100

    save the shit

--- brukerhistorieD.py
+++ brukerhistorieD.py
@@ -5,34 +5,56 @@ from datetime import datetime, timedelta
 con = sqlite3.connect('jernbane.db')
 cursor = con.cursor()
 
+# Finner Avgangstid på første stasjon i en Togrute basert på TogruteaID'en
+def avgangstidTogrute(TogruteID):
+    togruter = cursor.execute("""
+        SELECT StasjonerIRute.Avgangstid 
+        FROM Togrute NATURAL JOIN StasjonerIRute 
+        WHERE Togrute.TogruteID = :TogruteIDParam AND StasjonerIRuteAnkomsttid IS NULL
+    """, {"TogruteIDParam": TogruteID})
+
+    avgangstid = togruter.fetchall()
+    return 1
+
+con.create_function("avgangstidTogrute", 1, avgangstidTogrute)
+
 def finnTogruter():
     # Spør brukeren om input for stasjonsnavn, dato og klokkeslett
     print("Vennligst fyll ut ønsket startstasjon")
     stasjoner = stasjonsInput()
     dato = datoInput().date()
     datoDagEtter = dato + timedelta(days=1)
+    print(dato, datoDagEtter)
     klokkeslett = klokkeslettInput()
-    
-    # Finner TogruteID'en for Togruter som går mellom oppgitt start- og sluttstasjon,
-    # på oppgitt dato etter oppgitt klokkeslett
+
     togruter = cursor.execute("""
-        SELECT Togrute.TogruteID, startStasjonIRute.Avgangstid,  sluttStasjonIRute.Ankomsttid, Dato
-        FROM Togrute
-        JOIN StasjonerIRute AS startStasjonIRute ON Togrute.TogruteID = startStasjonIRute.TogruteID
-        JOIN StasjonerIRute AS sluttStasjonIRute ON Togrute.TogruteID = sluttStasjonIRute.TogruteID
-        JOIN TogruteForekomst ON Togrute.TogruteID = TogruteForekomst.TogruteID
-        WHERE (Dato = :dato OR Dato = :datoDagEtter)
-        AND startStasjonIRute.JernbanestasjonNavn = :startStasjon
-        AND sluttStasjonIRute.JernbanestasjonNavn = :sluttStasjon
-		AND  startStasjonIRute.StasjonsNr < SluttStasjonIRute.StasjonsNr
-        AND startStasjonIRute.Avgangstid >= :klokkeslett
+        SELECT Tog.TogruteID, startStasjonIRute.Avgangstid, sluttStasjonIRute.Ankomsttid, Dato
+        FROM Togrute AS Tog NATURAL JOIN StasjonerIRute AS startStasjonIRute
+             INNER JOIN StasjonerIRute AS sluttStasjonIRute ON Tog.TogruteID = sluttStasjonIRute.TogruteID
+             INNER JOIN TogruteForekomst ON Tog.TogruteID = TogruteForekomst.TogruteID
+        WHERE 
+            (CASE WHEN startStasjonIRute.Avgangstid < (
+                SELECT StasjonerIRute.Avgangstid 
+                FROM Togrute AS Tog2 NATURAL JOIN StasjonerIRute 
+                WHERE Tog.TogruteID = Tog2.TogruteID AND StasjonerIRute.Ankomsttid IS NULL
+            )
+            THEN DATE(Dato, "+1 day")
+            ELSE Dato END  = :dato AND startStasjonIRute.Avgangstid >= :klokkeslett)
+            AND startStasjonIRute.JernbanestasjonNavn = :startStasjon
+            AND sluttStasjonIRute.JernbanestasjonNavn = :sluttStasjon
+		    AND startStasjonIRute.StasjonsNr < SluttStasjonIRute.StasjonsNr   
 		ORDER BY Dato, startStasjonIRute.Avgangstid
-        """, {"dato": dato, "datoDagEtter": datoDagEtter, "startStasjon": stasjoner[0], "sluttStasjon": stasjoner[1], "klokkeslett": klokkeslett})
+        """, {
+            "dato": dato, "datoDagEtter": datoDagEtter, "startStasjon": stasjoner[0], 
+            "sluttStasjon": stasjoner[1], "klokkeslett": klokkeslett
+        })
+
     rows = togruter.fetchall()
 
+    if not rows:
+        print('''Det finnes ingen togruter som går mellom ''')
     for row in rows:
         print(f'''Tognr {row[0]} går fra {stasjoner[0]} kl {row[1]} og ankommer {stasjoner[1]} kl {row[2]} den {row[3]}''')
-
 
 con.commit()
 
@@ -50,7 +72,7 @@ def stasjonsInput():
     alleStasjonsNavn = jernbanestasjoner.fetchall()
 
     # Kaller funksjonen på nytt hvis inputen er ugyldig
-    if (startStasjon and sluttStasjon)  not in [navn[0] for navn in alleStasjonsNavn]:
+    if (startStasjon or sluttStasjon)  not in [navn[0] for navn in alleStasjonsNavn]:
         print("Ikke gyldig jernbanestasjoner, prøv igjen")
         stasjonsInput()
     elif startStasjon == sluttStasjon:
@@ -76,6 +98,5 @@ def klokkeslettInput():
         return klokkeslett
     print("Ikke gyldig klokkeslett, prøv igjen")
     return klokkeslettInput()
-
 
 finnTogruter()
